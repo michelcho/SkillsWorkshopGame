4c05abcaf639848a223edbd6423c9f9a2fabcc29
michelcho <michelcho92@hotmail.com>
2017-11-07 15:25:16 +0100

    Workshop

---
+++ WorkshopGameProgramming/Assets/Workshop/Scripts/Ability.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : ScriptableObject {
+
+    public string aName = "New Ability";
+    public Sprite aSprite;
+    public AudioClip aSound;
+    public float abaseCoolDown = 1f;
+
+    public abstract void Initialize(GameObject obj);
+    public abstract void TriggerAbility();
+       
+}
---
+++ WorkshopGameProgramming/Assets/Workshop/Scripts/RayCastAbility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Abilities/RaycastAbility")]
+public class RayCastAbility : Ability {
+
+    public int gunDamage = 1;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Color laserColor = Color.white;
+
+    private RaycastShootTriggerable rcShoot;
+   
+    public override void Initialize(GameObject obj)
+    {
+        rcShoot = obj.GetComponent<RaycastShootTriggerable>();
+        rcShoot.Initialize();
+
+        rcShoot.gunDamage = gunDamage;
+        rcShoot.weaponRange = weaponRange;
+        rcShoot.hitForce = hitForce;
+        rcShoot.laserLine.material = new Material(Shader.Find("Unlit/Color"));
+        rcShoot.laserLine.material.color = laserColor;
+        //throw new NotImplementedException();
+    }
+
+    public override void TriggerAbility()
+    {
+        rcShoot.Fire();
+        //throw new NotImplementedException();
+    }
+}
